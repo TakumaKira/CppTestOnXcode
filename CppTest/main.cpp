0fbe41296286104f58627a5d35927444468bf2ad
Takuma Kira <takuma@kirakiraworx.com>
2023-04-16 00:52:19 +0900

    Initial Commit

---
+++ CppTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CppTest
+//
+//  Created by Kira on 2023/04/16.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
